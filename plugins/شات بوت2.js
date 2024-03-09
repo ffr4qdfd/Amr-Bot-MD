5969b898a9da497462c704ee914efe60a7c46c47
ffr4qdfd <158234787+ffr4qdfd@users.noreply.github.com>
2024-03-09 15:29:43 +0200

    Add files via upload

---
+++ plugins/شات بوت2.js
@@ -0,0 +1,172 @@
+let handler = m => m 
+ handler.all = async function (m) { 
+ let chat = global.db.data.chats[m.chat] 
+  
+ if (/^احا$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `احتين علي احتك 🐦🥹`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+ } 
+    if (/^لا$/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `ازقو زق🐦`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+    } 
+
+if (/^العباسي$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `اي يحتة💕`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+}
+
+   if (/^عبيط|يعبيط|اهبل |غبي$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `انت يبيبي 🥲❤️`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+   }
+   
+if (/^كسمك$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `ابويا نط على امك ❤️`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+}
+   
+   if (/^بوت$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `اسمها .بوت يسطا انت نسيت النقطة هنفهم فيك لحد امته بقا😂`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+   }
+     if (/^نسوان/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `استغفر الله انا محترم 🥺`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+      }
+    if (/^الاء/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `العبيطة حج زياد دي ونن عينو م جوا🥺😂`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+      }
+    if (/^ضحك$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `يدوم يحب❤️‍🔥`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+    } 
+
+    if (/^بتعمل اي$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `انت مالك يحكاك 🐦😂`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+       if (/^زبري/i.test(m.text) ) { //sem prefixo 
+       conn.reply(m.chat, `منجا🐦`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+      
+    }
+ if (/^يب$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` قول طيب يحب 🫶🏻`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+      if (/^تيب|طيب$/i.test(m.text) ) { //sem prefixo 
+       conn.reply(m.chat, ` فرح امك قريب🫶🏻`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+       } 
+
+   if (/^بموت فيك$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat,'وانا كمان، متيجي نتجوز🥺',m) //wm, null, [['Menu', '#menu']], m) botones :v
+
+   }
+
+   if (/^اقلع$/i.test(m.text) ) { //sem prefixo
+     conn.reply(m.chat,'قلعت وانت لف واقلع ',m) //wm, null, [['Menu','#menu']], m) botones :v
+
+   }
+ if (/^متيجي ازنقك$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `مستنيك عند امك`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+
+ } 
+
+
+   }
+ if (/^هرميك من الشباك$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `خلاص خلاص مترمنيش 🥺💔`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+
+ } 
+  
+ if (/^امزح|بهزر$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` ماشي 🥲 `, m) //wm, null, [['Menu', '#menu']], m) botones :V
+   } 
+  
+ if (/^ف اي$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `فيه محشي وبانيه تاخد`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+ } 
+  
+ if (/^تست$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `تست تست ااالللوووو، هل تسمعني ححوولل`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+  
+ } 
+  
+ if (/^بتعمل اي دلوقت$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` باكل ميلكي رايز معه الملايكه`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^انا جيت$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` نورت البيت 😂❤️`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^اخرس$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `حاضر 🥺💔`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+    
+ } 
+  
+  
+ if (/^ملل|مللل|ملللل$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `عارفين ف اسكت احسن لك`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^السلام عليكم|السلام ورحمة الله وبركاته$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `وعليكم السلام ورحمة الله وبركاته `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^هات طبق$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` روح جيب لنفسك 🥲❤️`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+     
+ if (/^🤖$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `انت بوت عشان ترسل الاموجي ده يخنزير 🐦`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+     
+ if (/^🐦$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `🐦🐦🐦🐦🐦`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+  if (/^اي$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `احشرو ف الانتريه 🥲`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+   if (/^الو$/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `اشغلو`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+    } 
+   if (/^مين$/i.test(m.text) ) { //sem prefixo 
+   conn.reply(m.chat, `افسح التنين`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+   } 
+   if (/^العدوه$/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `بلد الميت متناك، واولهم لاكوست ومحمود`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+    } 
+   if (/^الفيوم$/i.test(m.text) ) { //sem prefixo 
+   conn.reply(m.chat, `نكتك ف حصه العلوم`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+   } 
+   if (/^هموت$/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `فيا 🌚`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+    } 
+   if (/^مطرطارس$/i.test(m.text) ) { //sem prefixo 
+   conn.reply(m.chat, `دخل فيك مطلعش خالص`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+   } 
+   if (/^يسطا$/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `خدتك ع البسطه، انت هتصاحبني يعلق`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+    } 
+     
+ if (/^نعم$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `حد ناداك 🌚🐦`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+
+  
+ if (/^كيفك|شخبارك|علوك|عامل ايه|عامل اي$/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, ` الحمد لله وانت`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+ return !0 } 
+ export default handler
